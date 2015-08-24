fee7fa06aeac95a46ace9d56bec7af402e17df77
Frank Juan <frank@conceptivestudio.com>
2015-08-24 14:38:31 -0400

    Ability to add and edit user bios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824172736) do
+ActiveRecord::Schema.define(version: 20150824175539) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20150824172736) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "location"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
