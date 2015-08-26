f56848a88e9fd261fbf3f79670ee708f521ebe82
Frank Juan <frank@conceptivestudio.com>
2015-08-26 01:29:38 -0400

    Home page setup with some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users, :only => [:show]
   resources :genre
   resources :jams do
   	resources :comments
   end
-  root 'jams#index'
+  root 'pages#home'
 end
