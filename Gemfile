9b92b42bd8c9527cf5e2677ab2fdb4c2847d7eac
Frank Juan <frank@conceptivestudio.com>
2015-09-14 23:31:02 -0400

    Add figaro and fog gems

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'puma'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'devise', '~> 3.5.2'
-gem "aws-sdk", "< 2.0"
+gem 'aws-sdk', '~> 1.36.1'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'paperclip', '~> 4.3.0'
 gem 'jquery-rails'
@@ -20,6 +20,7 @@ gem 'will_paginate', '~> 3.0.7'
 gem 'acts_as_follower', '~> 0.2.1'
 gem 'cancan'
 gem 'rails_12factor', group: :production
+gem 'fog'
 gem 'figaro', '~> 1.1.1'
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,17 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates :username, length: { in: 4..20 }
 
-  has_attached_file :avatar, styles: { medium: "250x250#", thumb: "100x100#" }, default_url: "/images/:style/missing.png",
-                    :bucket => 'jamseshpro',
-                    :storage => :s3,
-                    :s3_credentials => S3_CREDENTIALS
+  has_attached_file :avatar, styles: { medium: "250x250#", thumb: "100x100#" },
+                    default_url: "/images/:style/missing.png",
+                    :url  => ":s3_domain_url",
+                    :path => "public/avatars/:id/:style_:basename.:extension",
+                    :storage => :fog,
+                    :fog_credentials => {
+                        provider: 'AWS',
+                        aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+                        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+                    },
+                    fog_directory: ENV["FOG_DIRECTORY"]
 
   validates_attachment_content_type :avatar, :content_type => ['image/gif', 'image/jpeg', 'image/png', 'image/x-ms-bmp']
 
